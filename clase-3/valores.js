a22d236f5650de7aa807cbc31eb28871ffdaadb7
pikyr <pikytdv@gmail.com>
2021-12-07 20:41:00 -0300

    Fin de clase 3 - Tipos de valores

---
+++ clase-3/valores.js
@@ -0,0 +1,25 @@
+// Tipos de valores 
+// Valores primitivos
+// Se les llama primitivos por que son los tipos de valores basicos o fundamentales
+
+// Numero
+55
+
+// String
+"Hola coso"
+
+// Booleanos
+true 
+false
+
+// Valores vacios o placeholder
+// estos sirven para reservar un espacio en memoria
+null
+undefined //-Se debe evitar su uso
+
+// Valores no primitivos o de tipo objeto 
+// Array
+[1,2,3]
+
+// Objeto
+{nombre: "Comue"}
