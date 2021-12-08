8e48bb49917e508ffc4124a6616d9b06e076eefc
pikyr <pikytdv@gmail.com>
2021-12-08 18:10:58 -0300

    Fin de clase 4 - Declaracion e inicializacion de variables

---
+++ clase04-variables.js
@@ -0,0 +1,19 @@
+var name = "PikyR";
+
+// declarando una variable
+var numero; 
+
+// inicializando la variable 
+numero = 22;
+
+// array
+var arreglo = ["elemento1","elemento2","elemento3","etc"];
+
+// objeto
+var objeto = {
+  dato: "valor",
+  otroDato: "otroValor"
+}
+
+// Desde ECMAScript6 no se recomienda el uso de 'var'
+// en su lugar se utiliza 'let' y 'const'
