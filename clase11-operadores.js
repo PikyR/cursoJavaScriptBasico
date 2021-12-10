eafebb02f9be261aea0995362a062d42152b3232
pikyr <pikytdv@gmail.com>
2021-12-09 23:58:29 -0300

    Fin de clase 11 - Operadores

---
+++ clase11-operadores.js
@@ -0,0 +1,51 @@
+// - Operadores aritméticos u operadores binarios
+1 + 1;
+1 - 1;
+1 * 1;
+1 / 1;
+
+// - Operador de concatenación
+"texto " + "otro texto";
+
+// - Operador de negación u operador unitario
+!true; // false;
+
+// - Operador de asignación 
+let a = 99;
+
+// - Operadores de comparación
+5 == "5"; // true
+// solo compara el valor, no el tipo de dato
+
+5 === "5": // false
+// compara tanto valor como tipo de dato
+
+// Menor que 
+5 < 3; // false
+
+// Mayor que
+5 > 3; // true
+
+// Menor o igua
+5 <= 8 // true 
+
+// Mayor o igual
+5 >= 7 // false
+
+// And 
+// si las dos variables son 'true', el resultado de la comparacion es 'true' y si alguna de las variables no es 'true', el resultado es 'false'
+a && b; 
+
+// Or
+// si alguna de las variables es 'true', el resultado de la comparacion es 'true' y si ninguna variable es 'true', el resultado es 'false'
+a || b;
+
+// Operadores de incremento 
+let num = 10;
+num++; // 11
+num += 3; // 14
+
+// Operadores de decremento 
+let num = 20;
+num--; // 19
+num -= 3; // 16
