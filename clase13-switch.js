c138bafd726c479cdfd16d682028b552f37fce87
pikyr <pikytdv@gmail.com>
2021-12-14 18:28:36 -0300

    Fin de clase 13 - Switch case

---
+++ clase13-switch.js
@@ -0,0 +1,16 @@
+let num = 1;
+
+switch (num) {
+  case 1:
+    console.log("Uno!");
+    break;
+  case 10: 
+    console.log("El diego!");
+    break;
+  case 100:
+    console.log("Ajá cien");
+    break;
+  default:
+    console.log("No toy");
+    break;
+}
