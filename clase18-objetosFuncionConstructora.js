efc8770270a1780f444dc55f14183117f1b469f9
pikyr <pikytdv@gmail.com>
2021-12-17 21:13:21 -0300

    Fin de clase 18 - Objetos: 'Función constructora'

---
+++ clase18-objetosFuncionConstructora.js
@@ -0,0 +1,11 @@
+function coso(marca, modelo, annio) {
+  this.marca = marca;
+  this.modelo = modelo;
+  this.annio = annio;
+}
+
+var primerCoso = new coso("Teslo", "Modelo 1", 2020);
+
+var otroCoso = new coso("Teslo", "Modelo Y", 1999);
+
+var ultimoCoso = new coso("Toyeto", "Corona", 2019);
